dd1c167695d4a440cbd3a1fcd6d7f468ec31a245
grzegorz <cgrzegorz@gmail.com>
2020-07-27 15:02:40 -0700

    add function

--- tescjs.js
+++ tescjs.js
@@ -1,5 +1,10 @@
 console.log("branch master");
+let dupa = (value) =>{
+    console.log("to jest z brancha javascript");
+
+}
 let masterF = () => {
+    //to jest chuj wie co?
     console.log("master");
 }
 
