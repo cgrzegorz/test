b7a6219f16ab127e024624ebadb8e89558b7976a
grzegorz <cgrzegorz@gmail.com>
2020-07-27 15:09:38 -0700

    a

--- tescjs.js
+++ tescjs.js
@@ -1,7 +1,13 @@
 console.log("branch master");
+let s function(){
+    console.log("dupeczka");
+}
+
+console.log(masterF("to jest chuj wi c"));
+
+
+
 let masterF = () => {
     console.log("master");
     //to jest z mastera ale pierdol to
 }
-
-console.log(masterF("to jest chuj wi c"));
